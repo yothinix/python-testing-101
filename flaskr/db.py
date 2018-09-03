4ecc416858e739c5b7851fe53dea232905eeeed2
Yothin M <m@yothinix.com>
2018-09-03 19:23:11 +0700

    Add init_db script :rocket:

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,6 +1,8 @@
 import sqlite3
 
+import click
 from flask import current_app, g
+from flask.cli import with_appcontext
 
 
 def get_db():
@@ -19,3 +21,17 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    init_db()
+    click.echo('Initialized the database.')
