d46e1b655c6c6bfe99cd0d78a21cc6f66924389f
Yothin M <m@yothinix.com>
2018-09-03 19:06:27 +0700

    Application setup :rocket:

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from datetime import datetime
+
+from flask import jsonify, Flask, request
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+
+@app.route('/create')
+def create_budget():
+    data = request.get_json()
+    amount = data['amount']
+    date = data.get('date', datetime.now())
+
+    return jsonify(
+        dict(
+            amount=amount,
+            date=date
+        )
+    )
--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from main import hello
+from main import app, hello
 
 
 class TestHello(TestCase):
@@ -8,3 +8,15 @@ class TestHello(TestCase):
         expected = 'Hello World!'
         actual = hello()
         self.assertEqual(actual, expected)
+
+
+class TestCreateBudgetView(TestCase):
+    def test_create_budget_should_return_data(self):
+        expected = {'amount': 123, 'date': '2018-09-03T00:00:00'}
+        budget = {'amount': 123, 'date': '2018-09-03T00:00:00'}
+
+        with app.test_client() as client:
+            response = client.post('/create', json=budget)
+            actual = response.get_json()
+
+        self.assertDictEqual(actual, expected)
