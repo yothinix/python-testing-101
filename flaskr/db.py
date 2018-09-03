cd125965f86a9051514eba4e0e4835a0871d29a4
Yothin M <m@yothinix.com>
2018-09-03 19:26:19 +0700

    Add db initialize script to app :rocket:

--- flaskr/db.py
+++ flaskr/db.py
@@ -35,3 +35,8 @@ def init_db():
 def init_db_command():
     init_db()
     click.echo('Initialized the database.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
